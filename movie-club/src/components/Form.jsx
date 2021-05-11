3679a6f831c5f8f9c9063f533a166e7ff612e4fd
Malika Johnson <malikajohnson1995@MacBook-Air.local>
2021-05-11 11:30:32 -0400

    carousel beginnings

--- movie-club/src/components/Form.jsx
+++ movie-club/src/components/Form.jsx
@@ -7,9 +7,31 @@ import { baseURL, config } from "../services";
 function Form() {
   const [title, setTitle] = useState("")
   const [rating, setRating] = useState("")
-  const [comments, setComments] = useState("")
+  const [review, setReview] = useState("")
 
-  const params = useParams 
+  const params = useParams
+  
+  useEffect(() => {
+    if (params.id && props.movies.length) {
+      const movie = props.movies.find((movie) => movie.id === params.id);
+      if (movie) {
+        setTitle(movie.fields.title);
+        setRating(movie.fields.rating);
+        setReview(movie.fields.review);
+      }
+    }
+  }, [props.movie, params.id]);
+
+  const handleSubmit = async (e) => {
+    e. preventdefault()
+    const newReview = {
+      title,
+      rating,
+      review,
+    }
+
+    
+  }
 
   return (
     
@@ -19,24 +41,24 @@ function Form() {
         <input
           type="text"
           id="title"
-          value="{title}"
-          onChange={(e) => ()}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
         <label htmlFor="rating">Rating: </label>
         <input
           type="text"
           id="rating"
           value={rating}
-          onChange={(e) => ()}
+          onChange={(e) => setRating(e.target.value)}
         />
-        <label htmlFor="comments">Comments: </label>
+        <label htmlFor="review">Review: </label>
         <input
           type="text"
-          id="comments"
-          value={comments}
-          onChange={(e) => ()}
+          id="review"
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
         />
-
+          <button type="Post"></button>
       </form>
     </div>
   )
